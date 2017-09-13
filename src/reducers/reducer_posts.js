cc4c12cb833b4c018215b6d84b6bf0c2b0e62025
panchorestrepo <panchorestrepo@yahoo.com>
2017-09-13 14:04:42 -0500

    Refactoring

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,6 @@
 import _ from 'lodash';
 import { FETCH_POSTS, DELETE_POST, CREATE_POST, EDIT_POST, VOTE_POST } from '../actions';
 
-export const TIME_STAMP = "timeStamp";
-export const VOTE_SCORE = "voteScore";
-
 export default function(state = {}, action) {
    switch (action.type) {
        case DELETE_POST:
@@ -13,7 +10,6 @@ export default function(state = {}, action) {
         const posts = _.mapKeys(filterPosts,'id');
         return {...posts};
        case CREATE_POST:
-        console.log('create post',action)
         return {...state, [action.payload.id] : action.payload};
        case EDIT_POST: {
         const postId = action.payload.id;
